f8cc84ec70af5af4e732522a0673c5b44d1d198d
mmmmmtasty <mikjohnson@gmail.com>
2015-09-10 18:57:56 +0100

    Fully compatible with thesportsdb...apart from crazy naming

--- SportScanner.bundle/Contents/Code/__init__.py
+++ SportScanner.bundle/Contents/Code/__init__.py
@@ -8,6 +8,9 @@ netLock = Thread.Lock()
 successCount = 0
 failureCount = 0
 
+sport_map = { "EPL" : "English Premier League",
+              "F1" : "FIA Formula One World Championship" }
+
 MIN_RETRY_TIMEOUT = 2
 RETRY_TIMEOUT = MIN_RETRY_TIMEOUT
 TOTAL_TRIES   = 1
@@ -79,6 +82,9 @@ class SportScannerAgent(Agent.TV_Shows):
     match = re.match('^(.*?): (.*)', media.show)
     sport = match.group(1)
     show_title = match.group(2)
+    if show_title in sport_map:
+      Log("SS: Mapping {0} to {1}".format(show_title, sport_map[show_title]))
+      show_title = sport_map[show_title]
     Log("SS: Attempting to match {0}".format(show_title))
     url = "{0}search_all_leagues.php?s={1}".format(SPORTSDB_API, sport)
     try:
@@ -104,7 +110,7 @@ class SportScannerAgent(Agent.TV_Shows):
           )
         )
         match = True
-        Data.Save( "{0}.json".format(x['idLeague']), x)
+        Data.Save( "{0}-league.json".format(x['idLeague']), x)
         continue
 
     # See if anything else comes close if we are doing a deeper manual search and haven't found anything already
@@ -132,53 +138,61 @@ class SportScannerAgent(Agent.TV_Shows):
   def update(self, metadata, media, lang):
     Log("SS: update for: {0}".format(metadata.id))
 
-    #Get the zip archive for the show
+    #Get the zip archive for the show, preferably from disk
     try:
-      file_input = Data.Load("{0}.json".format(metadata.id))
+      file_input = Data.Load("{0}-league.json".format(metadata.id))
+      league_metadata = JSON.ObjectFromString(file_input)
     except:
-      #We need to go and get it from the web then
+      url = "{0}lookupleague.php?id={1}".format(SPORTSDB_API, metadata.id)
+      league_metadata = JSON.ObjectFromString(GetResultFromNetwork(url, True))['leagues'][0]
       pass
-    data = JSON.ObjectFromString(file_input)
 
     #Fill in any missing information for show and download posters/banners
-    metadata.title = data['name']
-    metadata.summary = data['summary']
-    metadata.genres = data['sport']
+    metadata.title = league_metadata['strLeague']
+    metadata.summary = league_metadata['strDescriptionEN']
+    metadata.genres.add(league_metadata['strSport'])
     try:
-     metadata.originally_available_at = data['originally_available_at']
+     metadata.originally_available_at = league_metadata['intFormedYear']
     except:
      pass
 
     #Work out what episodes we have and match them to ones in the right season
     @parallelize
     def UpdateEpisodes():
-
       #Go through available episodes
       for s in media.seasons:
-        # I don't believe either of these actually do anything
-        metadata.seasons[s].summary = data['seasons'][s]['summary']
-        metadata.seasons[s].title = data['seasons'][s]['name']
+        # Hack for thesportsdb adding the season onto the title of the event
+        suffix = " ({0} season)".format(s)
 
-        Log("SS: Downloading posters for {0}".format(data['seasons'][s]['name']))
-        # Download the episode thumbnail
-        valid_names = list()
+        # Get events for the season we care about
+        url = "{0}eventsseason.php?id={1}&s={2}".format(SPORTSDB_API, metadata.id, s)
+        season_metadata = JSON.ObjectFromString(GetResultFromNetwork(url, True))
 
-        if 'posters' in data['seasons'][s]:
-          for poster in data['seasons'][s]['posters']:
-            try:
-              metadata.seasons[s].posters[poster] = Proxy.Media(GetResultFromNetwork(poster, False))
-              valid_names.append(poster)
-            except:
-              Log("SS: Failed to add poster for {0}".format(s))
-              pass
-        else:
-          Log("SS: No season posters to download for {0}".format(s))
-
-        metadata.seasons[s].posters.validate_keys(valid_names)
+        # #There is currently no concept of a season having art in thesportsdb!
+        #
+        # # I don't believe either of these actually do anything
+        # metadata.seasons[s].summary = data['seasons'][s]['summary']
+        # metadata.seasons[s].title = data['seasons'][s]['name']
+        #
+        # Log("SS: Downloading posters for {0}".format(data['seasons'][s]['name']))
+        # # Download the episode thumbnail
+        # valid_names = list()
+        #
+        # if 'posters' in data['seasons'][s]:
+        #   for poster in data['seasons'][s]['posters']:
+        #     try:
+        #       metadata.seasons[s].posters[poster] = Proxy.Media(GetResultFromNetwork(poster, False))
+        #       valid_names.append(poster)
+        #     except:
+        #       Log("SS: Failed to add poster for {0}".format(s))
+        #       pass
+        # else:
+        #   Log("SS: No season posters to download for {0}".format(s))
+        #
+        # metadata.seasons[s].posters.validate_keys(valid_names)
 
         for e in media.seasons[s].episodes:
           episode = metadata.seasons[s].episodes[e]
-          season_metadata = data['seasons'][s]
           episode_media = media.seasons[s].episodes[e]
           try:
             Log("SS: Matching episode number %s: %s" % e, episode.title)
@@ -191,12 +205,22 @@ class SportScannerAgent(Agent.TV_Shows):
             best_score = 0
             #First try and match an episode
             Log("SS: Looking for match for %s" % episode_media.title)
-            for p in season_metadata['episodes']:
+            for p in range(len(season_metadata['events'])):
               #Log("SS: Comparing {0} to {1}".format(season_metadata['episodes'][p]['title'], episode_media.title))
-              if season_metadata['episodes'][p]['date'] and episode_media.originally_available_at:
-                if season_metadata['episodes'][p]['date'] == episode_media.originally_available_at:
-                  closeness = similar(season_metadata['episodes'][p]['title'], episode_media.title)
-                  Log("SS: Match ratio of {0} between {1} and {2}".format(1, episode_media.title, season_metadata['episodes'][p]['title']) )
+              if ("dateEvent" in season_metadata['events'][p]) and episode_media.originally_available_at:
+                if season_metadata['events'][p]['dateEvent'] == episode_media.originally_available_at:
+
+                  # This is grim!!!
+                  # I have to add a suffix with the season to more closely match what sportsdb records
+                  # We then have to remove it afterwards because it looks shit.
+                  match = re.match("/.*{0}$/".format(suffix), episode_media.title)
+                  if match:
+                    adj_title = episode_media.title
+                  else:
+                    Log("SS: Adjusting {0} to add season".format(episode_media.title))
+                    adj_title = episode_media.title + suffix
+                  closeness = similar(adj_title, season_metadata['events'][p]['strEvent'])
+                  Log("SS: Match ratio of {0} between {1} and {2}".format(closeness, adj_title, season_metadata['events'][p]['strEvent']) )
                   #If they are a perfect match then we are done
                   if closeness == 1:
                     best_score = 1
@@ -209,30 +233,32 @@ class SportScannerAgent(Agent.TV_Shows):
 
             #Only accept if the match is better than 80%
             if best_score > 0.8 and c:
-              Log("SS: Updating metadata for {0}".format(season_metadata['episodes'][c]['title']))
-              episode.title = season_metadata['episodes'][c]['title']
-              episode.summary = season_metadata['episodes'][c]['summary']
-              episode.originally_available_at = datetime.datetime.strptime(season_metadata['episodes'][c]['date'], "%Y-%m-%d").date()
+              Log("SS: Updating metadata for {0}".format(season_metadata['events'][c]['strEvent']))
+              episode.title = re.sub(suffix,"",season_metadata['events'][c]['strEvent'])
+              #TODO: I SHOULD BE WRITING A SUMMARY HERE
+              #episode.summary = season_metadata['events'][c]['summary']
+              episode.originally_available_at = datetime.datetime.strptime(season_metadata['events'][c]['dateEvent'], "%Y-%m-%d").date()
             else:
-              try:
-                Log("SS: Best match was %d" % best_score)
-              except:
-                pass
+              match = re.match("Unmatched:", episode.title)
+              if not match:
+                episode.title = "Unmatched: {0}".format(episode_media.title)
+              Log("SS: Best match was %d" % best_score)
               return
 
             Log("SS: Downloading thumbnail for {0}".format(episode.title))
             # Download the episode thumbnail
             valid_names = list()
-            if 'thumbs' in season_metadata['episodes'][c]:
-              for thumb in season_metadata['episodes'][c]['thumbs']:
-                try:
-                  episode.thumbs[thumb] = Proxy.Media(GetResultFromNetwork(thumb, False))
-                  valid_names.append(thumb)
-                except:
-                  Log("SS: Failed to add poster for {0}".format(episode.title))
-                  pass
+            if season_metadata['events'][c]['strThumb'] is not None:
+              thumb = season_metadata['events'][c]['strThumb']
+              # thumb = "{0}/preview".format(season_metadata['events'][c]['strThumb'])
+              try:
+                episode.thumbs[thumb] = Proxy.Media(GetResultFromNetwork(thumb, False))
+                valid_names.append(thumb)
+              except:
+                Log("SS: Failed to add thumbnail for {0}".format(episode.title))
+                pass
             else:
-              Log("SS: No season posters to download for {0}".format(episode.title))
+              Log("SS: No thumbs to download for {0}".format(episode.title))
 
             episode.thumbs.validate_keys(valid_names)
 
@@ -242,39 +268,58 @@ class SportScannerAgent(Agent.TV_Shows):
     @parallelize
     def DownloadImages():
       Log("Downloading Images")
-      # Add a download task for each image
-      i = 0
-      for banner_url in data['banners']:
-        i += 1
+      # Each image is stored separately so we have to do something strange here
+      if league_metadata['strBanner'] is not None:
+        banners_to_dl = list()
+        banners_to_dl.append(league_metadata['strBanner'])
+        for b in range(1, 10):
+          key_name = "strBanner{0}".format(b)
+          if key_name in league_metadata:
+            if league_metadata[key_name] is not None:
+              banners_to_dl.append(league_metadata[key_name])
+              # banners_to_dl.append("{0}/preview".format(league_metadata[key_name]))
+        for i in range(len(banners_to_dl)):
+          banner_url = banners_to_dl[i]
+          Log("SS: Downloading {0}".format(banner_url))
+          @task
+          def DownloadImage(metadata=metadata, banner_url=banner_url, i=i, valid_names=valid_names):
+
+            valid_names.append(banner_url)
+
+            if banner_url not in metadata.banners:
+              Log("SS: Downloading banner {0}".format(banner_url))
+              try:
+                metadata.banners[banner_url] = Proxy.Preview(GetResultFromNetwork(banner_url, False), sort_order=(i+1))
+              except:
+                Log("SS: Failed to set banner for {0}".format(metadata.title))
+                pass
+      else:
+        Log("SS: No banners to download for {0}".format(league_metadata['strLeague']))
+
+      fanart_to_dl = list()
+      for b in range(1, 10):
+        key_name = "strFanart{0}".format(b)
+        if key_name in league_metadata:
+          if league_metadata[key_name] is not None:
+            fanart_to_dl.append(league_metadata[key_name])
+            # fanart_to_dl.append("{0}/preview".format(league_metadata[key_name]))
+      for i in range(len(fanart_to_dl)):
+        fanart_url = fanart_to_dl[i]
+        Log("SS: Downloading {0}".format(fanart_url))
         @task
-        def DownloadImage(metadata=metadata, banner_url=banner_url, i=i, valid_names=valid_names):
+        def DownloadImage(metadata=metadata, fanart_url=fanart_url, i=i, valid_names=valid_names):
 
-          valid_names.append(banner_url)
+          valid_names.append(fanart_url)
 
-          if banner_url not in metadata.banners:
-            Log("SS: Downloading banner {0}".format(banner_url))
+          if fanart_url not in metadata.posters:
+            Log("SS: Downloading poster {0}".format(fanart_url))
             try:
-              metadata.banners[banner_url] = Proxy.Preview(GetResultFromNetwork(banner_url, False), sort_order=i)
-            except:
-              Log("SS: Failed to set banner for {0}".format(metadata.title))
-              pass
-
-      # Add a download task for each image
-      for poster_url in data['posters']:
-        @task
-        def DownloadImage(metadata=metadata, poster_url=poster_url, i=i, valid_names=valid_names):
-
-          valid_names.append(poster_url)
-
-          if poster_url not in metadata.posters:
-            Log("SS: Downloading poster {0}".format(poster_url))
-            try:
-              metadata.posters[poster_url] = Proxy.Preview(GetResultFromNetwork(poster_url, False), sort_order=i)
+              metadata.posters[fanart_url] = Proxy.Preview(GetResultFromNetwork(fanart_url, False), sort_order=(i+1))
             except:
               Log("SS: Failed to set poster for {0}".format(metadata.title))
               pass
 
-      # Check each poster, background & banner image we currently have saved. If any of the names are no longer valid, remove the image
-      metadata.posters.validate_keys(valid_names)
-      metadata.art.validate_keys(valid_names)
-      metadata.banners.validate_keys(valid_names)
+    # Check each poster, background & banner image we currently have saved. If any of the names are no longer valid, remove the image
+    metadata.posters.validate_keys(valid_names)
+    metadata.art.validate_keys(valid_names)
+    metadata.banners.validate_keys(valid_names)
